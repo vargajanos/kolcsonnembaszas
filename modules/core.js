0624ff6e21bab70f9445dca97662eeb3b6b37456
vargajanos <janosvarga706@gmail.com>
2024-11-10 15:15:03 +0100

    alap cuccok

--- modules/core.js
+++ modules/core.js
@@ -14,5 +14,87 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/reg', (req, res) => {
+    ejs.renderFile('./views/registration.ejs', { session: req.session }, (err, html)=>{
+        if (err){
+            console.log(err);
+            return
+        }
+        req.session.msg = '';
+        res.send(html);
+    });
+});
 
+router.get('/rent', (req, res)=>{
+    if (req.session.isLoggedIn){
+        ejs.renderFile('./views/rent.ejs', { session: req.session }, (err, html)=>{
+            if (err){
+                console.log(err);
+                return
+            }
+            req.session.msg = '';
+            res.send(html);
+        });
+        return
+    }
+    res.redirect('/');
+});
+
+router.get('/stat', (req, res)=>{
+    if (req.session.isLoggedIn){
+        ejs.renderFile('./views/statistics.ejs', { session: req.session }, (err, html)=>{
+            if (err){
+                console.log(err);
+                return
+            }
+            req.session.msg = '';
+            res.send(html);
+        });
+        return
+    }
+    res.redirect('/');
+});
+
+router.get('/me', (req, res)=>{
+    if (req.session.isLoggedIn){
+        ejs.renderFile('./views/user.ejs', { session: req.session }, (err, html)=>{
+            if (err){
+                console.log(err);
+                return
+            }
+            req.session.msg = '';
+            res.send(html);
+        });
+        return
+    }
+    res.redirect('/');
+});
+
+router.get('/admin', (req, res)=>{
+    if (req.session.isLoggedIn){
+        ejs.renderFile('./views/admin.ejs', { session: req.session }, (err, html)=>{
+            if (err){
+                console.log(err);
+                return
+            }
+            req.session.msg = '';
+            res.send(html);
+        });
+        return
+    }
+    res.redirect('/');
+});
+
+router.get('/logout', (req, res)=>{
+
+    req.session.isLoggedIn = false;
+    req.session.userID = null;
+    req.session.userName = null;
+    req.session.userEmail = null;
+    req.session.userRole = null;
+    req.session.msg = 'You are logged out!';
+    req.session.severity = 'info';
+    res.redirect('/');
+
+});
 module.exports = router;
--- modules/user.js
+++ modules/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('./database');
 const moment = require('moment');
+var CryptoJS = require("crypto-js");
 
 router.post('/reg', (req,res)=>{
     let {name, email, password, confirm} = req.body;
@@ -48,5 +49,44 @@ router.post('/reg', (req,res)=>{
     })
 
 })
+
+router.post('/login', (req, res)=>{
+    let { email, password } = req.body;
+
+    if (!email || !password) {
+        req.session.msg = 'Missing data!';
+        req.session.severity = 'danger';
+        res.redirect('/');
+        return
+    }
+
+    db.query(`SELECT * FROM users WHERE email=? AND password=?`, [email, CryptoJS.SHA1(password).toString()], (err, results)=>{
+        if (err){
+            req.session.msg = 'Database error!';
+            req.session.severity = 'danger';
+            res.redirect('/');
+            return
+        }
+        if (results == 0){
+            req.session.msg = 'Invalid credentials!';
+            req.session.severity = 'danger';
+            res.redirect('/');  
+            return
+        }
+        req.session.msg = 'You are logged in!';
+        req.session.severity = 'info';
+
+        req.session.isLoggedIn = true;
+        req.session.userID = results[0].ID;
+        req.session.userName = results[0].name;
+        req.session.userEmail = results[0].email;
+        req.session.userRole = results[0].role;
+
+        console.log(req.session);
+        res.redirect('/rent');
+        return
+    });
+});
+
 
 module.exports = router;
